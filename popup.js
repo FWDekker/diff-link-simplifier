bd6c08edaa4506399758073cfc635c329cdf38e6
Felix W. Dekker <felix@fwdekker.com>
2019-09-03 13:26:07 +0200

    Add "copy" button to interface

--- popup.js
+++ popup.js
@@ -1,13 +1,40 @@
 // Reusable refs
 const simpleUrlOutput = document.getElementById("simpleUrlOutput");
+const simpleUrlCopy = document.getElementById("simpleUrlCopy");
+const simpleUrlCopiedMessage = document.getElementById("simpleUrlCopiedMessage");
 const wikiUrlOutput = document.getElementById("wikiUrlOutput");
+const wikiUrlCopy = document.getElementById("wikiUrlCopy");
+const wikiUrlCopiedMessage = document.getElementById("wikiUrlCopiedMessage");
 
 // Select all text on click
-simpleUrlOutput.onclick = function() { simpleUrlOutput.select() };
-wikiUrlOutput.onclick = function() { wikiUrlOutput.select() };
+simpleUrlOutput.onclick = () => simpleUrlOutput.select();
+wikiUrlOutput.onclick = () => wikiUrlOutput.select();
+
+// Copy to clipboard on click
+const writeToClipboard = text => {
+    navigator.clipboard.writeText(text)
+        .then(() => {})
+        .catch(err => {
+            console.log("Failed to write to clipboard.", err);
+        });
+};
+
+simpleUrlCopy.onclick = () => {
+    simpleUrlOutput.select();
+    writeToClipboard(simpleUrlOutput.value);
+    wikiUrlCopiedMessage.style.visibility = "hidden";
+    simpleUrlCopiedMessage.style.visibility = "visible";
+};
+
+wikiUrlCopy.onclick = () => {
+    wikiUrlOutput.select();
+    writeToClipboard(wikiUrlOutput.value);
+    simpleUrlCopiedMessage.style.visibility = "hidden";
+    wikiUrlCopiedMessage.style.visibility = "visible";
+};
 
 // Add contents to input boxes
-chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function (tabs) {
+chrome.tabs.query({"active": true, "lastFocusedWindow": true}, tabs => {
     const originalUrl = new URL(tabs[0].url);
     const queryParams = new URLSearchParams(originalUrl.search);
 
